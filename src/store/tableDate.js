cf30d2cd9b23d34dade1f427874f3a4d68b4ba65
ganster234 <1091251733@qq.com>
2025-12-26 14:44:26 +0800

    26

--- src/store/tableDate.js
+++ src/store/tableDate.js
@@ -1,32 +1,479 @@
-import dayjs from "dayjs";
-// el-tag / el-button ：primary ， success ， info，warning，danger
-export const headerTbale = [
-  {
-    prop: "username",
-    label: "名字",
-
+export const userInfo = {
+  name: "郑凯文",
+  job: "前端开发工程师",
+  avatar: {
+    cdn: "https://img.meituan.net/portalweb/1990b60eb000fc2f41488fe75a0b333e77897.jpg",
+    local: "/avatar.jpg",
   },
-  {
-    prop: "integralId",
-    label: "描述",
-    minWidth: "70",
-    showOverflowTooltip: true, //是否腻出加省略号
-  },
-  {
-    prop: "username",
-    label: "状态",
-    render: (value) => {
-      return {
-        isTag: true, //是否显示标签
-        content: value !== "plm852456" ? "12" : "禁用",
-        type: value !== "plm852456" ? "primary" : "warning",
-      };
+  infoList: [
+    { title: "年龄", value: "26" },
+    { title: "性别", value: "男" },
+    { title: "电话", value: "18048429819" },
+    { title: "邮箱", value: "1091251733@qq.com" },
+    { title: "期望薪资", value: "11-13 k" },
+    { title: "毕业院校", value: "西南交通大学" },
+  ],
+  employmentList: [
+    {
+      startDate: "2020-09",
+      endDate: "2022-09",
+      title: "派沃特科技有限公司",
+      value: ["· 前端开发工程师 | 7K"],
     },
-  },
-  // dayjs转换
-  {
-    prop: "createTime",
-    label: "时间",
-    render: (value) => dayjs(value).format("YYYY-MM-DD HH:mm:ss") || "-",
-  },
-];
+    {
+      startDate: "2022-10",
+      endDate: "2023-05",
+      title: "含然软件有限公司",
+      value: ["· 前端开发工程师 | 8.5K"],
+    },
+    {
+      startDate: "2023-05",
+      endDate: "2023-11",
+      title: "成都雪域云巅科技有限公司",
+      value: ["· 前端开发工程师 | 9K"],
+    },
+    {
+      startDate: "2023-11",
+      endDate: "至今",
+      title: "成都谦屿科技有限公司",
+      value: ["· 前端开发工程师 | 11K"],
+    },
+  ],
+  skillExpertise: [
+    '1. 熟练使用主流框架及自动化构建工具 <span class="font-bold text-black">Vue2/3、React、Electron、Jquery、Uniapp。Vite、Webpack</span>',
+    '2. 熟练使用主流UI库 <span class="font-bold text-black">HeroUI、HiUIDesign、Vant、AntDesign/Mobile、uView、uCharts、ECharts、ElementUI、NaiveUI</span>',
+    "3. 对组件化、脚手架开发有着组合丰富的 项目经验，模块化开发以及代码封装思想。",
+    '状态管理库：<span class="font-bold text-black">Zustand</span>、Vuex、Pinia',
+    '4. 掌握多种后台交互技术，熟练使用Axios、Fetch、Request、Ajax、<span class="font-bold text-black">SSE、WebSocket</span>请求数据；',
+    "5. 掌握使用Flex、Grid一二维布局进行开发，能在项目中使用ES6以上(赋值与解构赋值、Promise、箭头函数、rest运算符等)语法简化程序；",
+    "6. 掌握HTML、CSS等网页前端技术，可纯手写可读性强且符合标准的页面，快速搭建前端界面并对浏览器渲染原理一定了解；",
+    "7. 掌握JavaScript/TypeScript开发，熟悉JS封装、类、原型链、闭包等代码的编写；",
+    '8. 熟悉Less、Sass的Css预处理器，<span class="font-bold text-black">Tailwind CSS、Framer-motion</span>、Animatopy，能够实现Css代码优化可读性强、增强界面交互；',
+    "9. 熟练原生小程序及App、H5多端开发。以及响应式页面开发",
+    "10. 了解 Node.js开发，能够使用Node js和 Express实现服务器端功能,数据库的操作及其 增、删、改、查等基本代码的编写",
+  ],
+  projectExperienceList: [
+    {
+      project: "东孚ERP + 东孚官网",
+      titleURl: "官网地址：",
+      weburl: "https://www.dongfufood.net/",
+      summary: `该项目是为辽宁东孚控股公司开发的erp+官网系统，官网主要是介绍东孚公司的核心价值观，以及品质保障和各种产品介绍。
+erp系统的模块有:登录页；后台首页(后台功能预览)；商品管理模块(查询商品、供货、发货管理)；订单管理(查询订单信息及修改)；
+OA管理；仓库管理；商品管理；CRM管理；销售管理(对销售订单，商品销售情况进行可视化报表分析)；账号管理(超级管理员可以
+添加普通管理员账号，以及查看账号信息)；帮助中心模块包括操作指南。`,
+      dutiesList: [
+        "负责开发官网",
+        "负责考勤打卡小程序开发，可视化大屏系统开发",
+        "确定ERP技术选型，规划工作时间搭建架构及人员工作安排。",
+        "负责ERP生产，商品，采购，仓库，销售，账号管理，OA等模块开发",
+      ],
+      techStack:
+        "React、Tailwindcss、Zustand、WebSocket、TypeScript、React-Router、Vite、Fetch、AntDesign、HeroUI",
+      imageList: [
+        {
+          cdn: "https://img.meituan.net/portalweb/7dff236761d6249bfb4f405db6c59a4f213265.png",
+          local: "/dongfu/0.png",
+        },
+        {
+          cdn: "https://img.meituan.net/portalweb/d8406ad322d676b332e6b0d6e49fae8d274536.png",
+          local: "/dongfu/1.png",
+        },
+        {
+          cdn: "https://img.meituan.net/portalweb/a8747b5cdc22cca5e288c5946da3a916153990.png",
+          local: "/dongfu/2.png",
+        },
+        {
+          cdn: "https://img.meituan.net/portalweb/ebe7575dc55ada7475fd8b3c8dd3259c226595.png",
+          local: "/dongfu/3.png",
+        },
+        {
+          cdn: "https://img.meituan.net/portalweb/19b6ed8896268011ea9d712365b29949200585.png",
+          local: "/dongfu/4.png",
+        },
+        {
+          cdn: "https://img.meituan.net/portalweb/5c96507e4f57de93ce760e16c678df70338177.png",
+          local: "/dongfu/5.png",
+        },
+        {
+          cdn: "https://img.meituan.net/portalweb/25ce7ed2ad4bc62dc7de6d2f6f0261ee410578.png",
+          local: "/dongfu/6.png",
+        },
+        {
+          cdn: "https://img.meituan.net/csc/09a592ededaf8ab012f049e75058841f44962.png",
+          local: "/dongfu/7.png",
+        },
+        {
+          cdn: "https://img.meituan.net/portalweb/f535c0ea2129b34fdecb65930a0b2b0145433.png",
+          local: "/dongfu/8.png",
+        },
+        {
+          cdn: "https://img.meituan.net/portalweb/1be753cdb69c4d8c26bcdca9fae8527b54302.png",
+          local: "/dongfu/9.png",
+        },
+        {
+          cdn: "https://img.meituan.net/portalweb/f8fc2d7e5114c40283b2b9c3379b427578713.png",
+          local: "/dongfu/10.png",
+        },
+      ],
+    },
+    {
+      project: "东孚考勤打卡管理小程序",
+      summary: `此项目借鉴钉钉APP而开发的企业级打卡、管理 小程序，方便员工上下班打卡统计考勤记录，PC端部分功能平移小程序内（方便用户在手机端能管理企业部分业务）`,
+      dutiesList: ["负责整个项目前端开发、后期维护迭代"],
+      techStack: "Uniapp、Vue3、Less、uView",
+      h5ImageList: [
+        {
+          cdn: "https://img.meituan.net/portalweb/316cbff13fbe17256b71c1f90e0de2cd31119.png",
+          local: "/dongfu/dak/0.png",
+        },
+        {
+          cdn: "https://img.meituan.net/portalweb/b35c1c97318709c88df7d4986802860022990.png",
+          local: "/dongfu/dak/1.png",
+        },
+        {
+          cdn: "https://img.meituan.net/portalweb/b16903822db4c8699b7912ab894c704124855.png",
+          local: "/dongfu/dak/2.png",
+        },
+        {
+          cdn: "https://img.meituan.net/portalweb/4867dbd1bf5f90402728a1ba7ba3026a25431.png",
+          local: "/dongfu/dak/3.png",
+        },
+        {
+          cdn: "https://img.meituan.net/portalweb/539bc28d56265e434f0c270e813275b744084.png",
+          local: "/dongfu/dak/4.png",
+        },
+        {
+          cdn: "https://img.meituan.net/portalweb/05f4b36c901b437624de35825967b67419288.png",
+          local: "/dongfu/dak/5.png",
+        },
+        {
+          cdn: "https://img.meituan.net/portalweb/b292dfae379ad56f3cf2e624d90bffbd47004.jpg",
+          local: "/dongfu/dak/6.png",
+        },
+        {
+          cdn: "https://img.meituan.net/portalweb/ed7bc042e5b8ca1e0415a1a149b5155820400.png",
+          local: "/dongfu/dak/7.png",
+        },
+        {
+          cdn: "https://img.meituan.net/portalweb/3dc047b0e9874d4cf497a73c76f9286021991.png",
+          local: "/dongfu/dak/8.png",
+        },
+        {
+          cdn: "https://img.meituan.net/portalweb/9719fb084074f0a45427ff8b7e20620a31638.png",
+          local: "/dongfu/dak/9.png",
+        },
+        {
+          cdn: "https://img.meituan.net/portalweb/e2508ff9846d1c55f604dd1ebe5d9a7b26238.png",
+          local: "/dongfu/dak/10.png",
+        },
+      ],
+    },
+    {
+      project: "UiTab (浏览器插件新标签页、后台管理系统、官网）",
+      titleURl: "地址：",
+      weburl: "https://uitab.com/",
+      summary: `公司为了方便大量用户使用浏览器更方便、有交互体验感（而诞生的一个项目）。如天气、日历、AI、伴随AI、下班倒计时、今
+日新闻、代办、备忘录等小组件，即点即用方便快捷。实现小组件在页面任意拖拽，点击切换任意调整大小形
+态，以及页面随着壁纸变化而变化的高度定制化主题系统，白夜模式切换。而与用户高度定制化一个项目`,
+      dutiesList: [
+        "负责官网、后台开发",
+        "浏览器插件负责所有功能性小组件开发",
+        "插件搜索功能、AI聊天开发",
+      ],
+      techStack:
+        "React、Cocos、Vite、TypeScript、Zustand、Dayjs、SSE、framer-motion、Immer。NextUI、AntDesign、Tailwindcss、Highlighter",
+      webImageList: [
+        {
+          cdn: "https://img.meituan.net/portalweb/409f74a70bc04669977df77264660360173161.png",
+          local: "/uitab/0.png",
+        },
+        {
+          cdn: "https://img.meituan.net/portalweb/26d934fdb625c04315b324b91a27ee13350011.png",
+          local: "/uitab/1.png",
+        },
+        {
+          cdn: "https://img.meituan.net/portalweb/08822b701146df270d85a6e024042f59501028.png",
+          local: "/uitab/2.png",
+        },
+        {
+          cdn: "https://img.meituan.net/portalweb/7572455dc2cefbe9190b87cac62164d6389201.png",
+          local: "/uitab/3.png",
+        },
+        {
+          cdn: "https://img.meituan.net/portalweb/b9b4efe2639c3d39c562635f8cd24794410664.png",
+          local: "/uitab/4.png",
+        },
+        {
+          cdn: "https://img.meituan.net/portalweb/e345ce6f733f355f2de11910f946e9ee185934.png",
+          local: "/uitab/10.png",
+        },
+
+        {
+          cdn: "https://img.meituan.net/portalweb/70888f73a2ec68effa8befb9a99d0867459428.png",
+          local: "/uitab/5.png",
+        },
+
+        {
+          cdn: "https://img.meituan.net/portalweb/242aa99bf9e0298377f3d22076cdd954235245.png",
+          local: "/uitab/6.png",
+        },
+        {
+          cdn: "https://img.meituan.net/portalweb/29dd18579826670435e8fe9f2cccf22f1261203.png",
+          local: "/uitab/7.png",
+        },
+        {
+          cdn: "https://img.meituan.net/portalweb/e59638b4047b7c14c9af8b13c1842dc2236589.png",
+          local: "/uitab/8.png",
+        },
+        {
+          cdn: "https://img.meituan.net/portalweb/e085441c089bf9a69282eeebade57cc6255895.png",
+          local: "/uitab/9.png",
+        },
+      ],
+    },
+    {
+      project: "自由行HTTP",
+      titleURl: "地址：",
+      weburl: "https://www.ziyxip.com/",
+      summary: `企业级代理IP服务商，全方位大数据采集，HTTP/HTTPS/SOCKS5/隧道代理/动态IP/固态IP`,
+      dutiesList: ["负责整个项目前端开发、后期维护迭代"],
+      techStack:
+        "React、Vite、TypeScript、Autofit、Zustand、Dayjs、Immer。Heroui、Framer-motion、Tailwindcss、Sweetalert2",
+      webImageList: [
+        {
+          cdn: "https://img.meituan.net/portalweb/c9df6798c8452890d3564350c82c4ec7129690.png",
+          local: "/ziyxip/0.png",
+        },
+        {
+          cdn: "https://img.meituan.net/portalweb/416c48464c498b703e94328cacd10ff755559.png",
+          local: "/ziyxip/1.png",
+        },
+        {
+          cdn: "https://img.meituan.net/portalweb/ab39b1ff6a433ba73e3e405862aa091570491.png",
+          local: "/ziyxip/7.png",
+        },
+        {
+          cdn: "https://img.meituan.net/portalweb/7cbd31e75297bd970c8251aa66f25818117823.png",
+          local: "/ziyxip/2.png",
+        },
+        {
+          cdn: "https://img.meituan.net/portalweb/fed1f4523fee0e4181a8dad8081c1b8a64121.png",
+          local: "/ziyxip/3.png",
+        },
+        {
+          cdn: "https://img.meituan.net/portalweb/3299a849e799ade095a45918c45843e933241.png",
+          local: "/ziyxip/4.png",
+        },
+        {
+          cdn: "https://img.meituan.net/portalweb/8e6f16f1ea6fc8226294691e455db18128713.png",
+          local: "/ziyxip/5.png",
+        },
+        {
+          cdn: "https://img.meituan.net/portalweb/5732c11d9139e1e0e6cd38093aea228942127.png",
+          local: "/ziyxip/6.png",
+        },
+
+        {
+          cdn: "https://img.meituan.net/portalweb/9b19b4e8a42a61de6ce629d51bdfc9f367019.png",
+          local: "/ziyxip/8.png",
+        },
+      ],
+    },
+    {
+      project: "天马云客服系统",
+      summary: `提供企业内部与客户聊天系统`,
+      dutiesList: ["负责整个项目前端开发、后期维护迭代"],
+      techStack:
+        "React、Vite、TypeScript、Autofit、Zustand、Dayjs、Immer。Heroui、Framer-motion、Tailwindcss、Sweetalert2",
+
+      h5ImageList: [
+        {
+          cdn: "https://img.meituan.net/portalweb/e07db7f16c6b774c4ab20be25bc3dc2c98441.png",
+          local: "/tianma/0.png",
+        },
+        {
+          cdn: "https://img.meituan.net/portalweb/8522fb50ac959ac2047fa03e00775c2673932.png",
+          local: "/tianma/1.png",
+        },
+        {
+          cdn: "https://img.meituan.net/portalweb/88f97ac9a41a380e1e9017c8dee6a5a342008.png",
+          local: "/tianma/2.png",
+        },
+        {
+          cdn: "https://img.meituan.net/portalweb/dddeddfe7a309dce37e7e0721f1ac20b29144.png",
+          local: "/tianma/3.png",
+        },
+        {
+          cdn: "https://img.meituan.net/portalweb/bb3547e26f374f76b3762ca84f278c3326467.png",
+          local: "/tianma/4.png",
+        },
+        {
+          cdn: "https://img.meituan.net/portalweb/7dbc66ac764c06884df693bf5bce47e939427.png",
+          local: "/tianma/5.png",
+        },
+        {
+          cdn: "https://img.meituan.net/portalweb/7f66e3195a266f5c39e501b3849a282a29545.png",
+          local: "/tianma/6.png",
+        },
+        {
+          cdn: "https://img.meituan.net/portalweb/cbc7cedc6ea8aaf548a77793e454255a27276.png",
+          local: "/tianma/7.png",
+        },
+        {
+          cdn: "https://img.meituan.net/portalweb/02de7d0d5bd0fa982a0829d0d262849c25421.png",
+          local: "/tianma/8.png",
+        },
+        {
+          cdn: "https://img.meituan.net/portalweb/5faca341c1de73cdba9f72ee7599190727121.png",
+          local: "/tianma/9.png",
+        },
+      ],
+    },
+    {
+      project: "公牛电子签名管理系统",
+      summary: `Bull系统包括C端和B端。C端专注于客户合同签字，通过<iframe>内嵌Shark系统。B端分为统计、模板和合同查询三个板 块。利用Echars展示不同贷款品牌统计和已签合同数量及比例。编辑不同贷款品牌的证书、合同。查询详情并提供下载签名证书 和完整合同的电子版。`,
+      dutiesList: [
+        "1.独立负责项目从零到一的全面开发，结合项目经理提供的原型图和业务需求，进行界面调整与开发。",
+        "2.为适配美国律师编辑习惯，使用Tiptap插件开发。通过二次编辑源码解决原插件表格颜色及样式丢失问题。",
+        "3.富文本编辑独立成组件，添加图片内联外联切换、可选表格无边框、重写预设的各类选择器，并在其它项目应用。",
+        "4.严谨的操作逻辑，防止误删文本变量、发送预览邮件、编辑已发布合同先创建副本供用户编辑，发布后才替换。",
+        "5.采用微前端技术对复杂项目进行解耦。",
+      ],
+      techStack: "Vue.js 2、VueX、Vue-Router、Vue-CLI、Vuetify-UI、Sass",
+      webImageList: [
+        {
+          cdn: "https://img.meituan.net/portalweb/7ece922870abebe8b7a7723f520aee9081550.webp",
+          local: "/gongniu/0.png",
+        },
+        {
+          cdn: "https://img.meituan.net/portalweb/c5c3fc3d665f3a65be1c342a3c3896e125790.png",
+          local: "/gongniu/1.png",
+        },
+        {
+          cdn: "https://img.meituan.net/portalweb/1a57b1950244c6ff0afe6ab1eb5527fd96736.png",
+          local: "/gongniu/2.png",
+        },
+        {
+          cdn: "https://img.meituan.net/portalweb/c537ba438865fb8df06053211edc2a5831882.png",
+          local: "/gongniu/3.png",
+        },
+        {
+          cdn: "https://img.meituan.net/portalweb/4431700fc6d8d434f9593c8eee8d581d121512.png",
+          local: "/gongniu/4.png",
+        },
+        {
+          cdn: "https://img.meituan.net/portalweb/b33d7f522646c8638711e15726eb66e222054.png",
+          local: "/gongniu/5.png",
+        },
+        {
+          cdn: "https://img.meituan.net/portalweb/4e969dc1d5a5eb54977afa7a3b64e71b28604.png",
+          local: "/gongniu/6.png",
+        },
+        {
+          cdn: "https://img.meituan.net/portalweb/f5d2a59c74856f364f3dec49a42f475542907.png",
+          local: "/gongniu/7.png",
+        },
+        {
+          cdn: "https://img.meituan.net/portalweb/fe4986c74a96c73d4628d4cddb2cf94f51433.png",
+          local: "/gongniu/8.png",
+        },
+      ],
+    },
+    {
+      project: "慕映影视（H5、后台系统）",
+      summary: `这是一个帮助影迷发现和管理影视作品的智能工具。它不仅能根据你的喜好推荐剧集，还能让你轻松记录“在看”、“想看”和“已看”的片单，并管理自己的观看进度。
+      类似于腾讯视频的视频播放软件。项目主要模块有首页（视频分类，视频筛选，视频搜索等），短视频，VIP会员，个人中心等`,
+      dutiesList: ["负责整个项目前端开发"],
+      techStack:
+        "Dlayer、Mescroll、React、Vite、TypeScript、Zustand、Heroui、Tailwindcss",
+      webImageList: [
+        {
+          cdn: "https://img.meituan.net/portalweb/e4bd6cc1f0cc0046609166163e5fa192637284.png",
+          local: "/muying/ht0.png",
+        },
+        {
+          cdn: "https://img.meituan.net/portalweb/df78b34a06042c7ad8679437ecde5a86225752.png",
+          local: "/muying/ht1.png",
+        },
+        {
+          cdn: "https://img.meituan.net/portalweb/91e802b0479ea6ccc383910c0075d241269434.png",
+          local: "/muying/ht2.png",
+        },
+        {
+          cdn: "https://img.meituan.net/portalweb/8c8fc2f26fdd6d226ffc89f190aaab3b160960.png",
+          local: "/muying/ht3.png",
+        },
+        {
+          cdn: "https://img.meituan.net/portalweb/0bf5854b74d142240ff76791ebeb1773166837.png",
+          local: "/muying/ht4.png",
+        },
+        {
+          cdn: "https://img.meituan.net/portalweb/8e0ede4f82345cf4abb6eb976a27c50991517.png",
+          local: "/muying/ht5.png",
+        },
+      ],
+      h5ImageList: [
+        {
+          cdn: "https://img.meituan.net/portalweb/97cd9539c07721ad69311358ba5f0f29248434.png",
+          local: "/muying/0.png",
+        },
+        {
+          cdn: "https://img.meituan.net/portalweb/3e72045f78eccca1801ce2a8484db2f2110036.png",
+          local: "/muying/1.png",
+        },
+        {
+          cdn: "https://img.meituan.net/portalweb/cbf8a6bea7c63a7b66b97e5b38c76ae583778.png",
+          local: "/muying/2.png",
+        },
+        {
+          cdn: "https://img.meituan.net/portalweb/989e85e98c87a464553579b1070c5eee48960.png",
+          local: "/muying/3.png",
+        },
+        {
+          cdn: "https://img.meituan.net/portalweb/03fdbd01d773fff69a442bbfbada2f1a88670.png",
+          local: "/muying/4.png",
+        },
+        {
+          cdn: "https://img.meituan.net/portalweb/2a9cf6eb0840e1f57bd5fee9f9c64db1172596.png",
+          local: "/muying/5.png",
+        },
+        {
+          cdn: "https://img.meituan.net/portalweb/e9f3a7fca20708c1932f2b0df2bfd668103032.png",
+          local: "/muying/6.png",
+        },
+        {
+          cdn: "https://img.meituan.net/portalweb/b4662194b63210ea3d79c59f0e34104f51082.png",
+          local: "/muying/7.png",
+        },
+        {
+          cdn: "https://img.meituan.net/portalweb/d17ddd22465a6070b6e7b04196e5c09642802.png",
+          local: "/muying/8.png",
+        },
+      ],
+    },
+    {
+      project: "流量汇,领新宝(App、后台管理系统)",
+      titleURl: "地址：APK",
+      summary: `流量汇VIP用户支付任务金额，发布视频任务。试列:抖音关注点赞等一系列任务。领新宝App用户接受并完成
+任务获得佣金，App加入广告联盟。此项目实现用户流量变现，平台流量变现主要业务场景`,
+      dutiesList: ["负责整个项目前端开发"],
+      techStack: "Uniapp，Vue3，Vant，Elementplus，JsonBig",
+    },
+    {
+      project: "仁泽到家 (PC、小程序、APP、H5)",
+      weburl: "https://www.renzedaojia.com/",
+      summary: `该项目是公司利用人群大量需求，在家设备、电视、家电等。在线下单师傅上门安装与维修的平台，方便用户
+即刻下单24小时内师傅到家送货上门、维修、安装等业务场景`,
+      dutiesList: ["负责整个项目前端开发、UI设计"],
+      techStack: "Vue2、Vuex、Uniapp、ElementUI、uView、Less、Axios、mixin",
+    },
+    {
+      project: "公益志愿者平台（微信小程序）",
+      summary: `(腾讯外包) 公益志愿者平台是腾讯公益慈善基金会主导的为各类公益活动展示举办与报名的小程序，具有公
+益活动首页、活动检索、活动详情、活动报名、个人公益中心等多个模块构成、致力于让更多人加入公益活动
+帮助社会弱势群体并且带来温暖`,
+      dutiesList: ["负责整个项目前端开发"],
+      techStack: "Uniapp，Vue3",
+    },
+  ],
+}
